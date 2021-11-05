3c867e90b0eea2b41ae56647eb99b28e00858dba
CmaillaKurup <cami670v@zbc.dk>
2021-11-05 08:51:49 +0100

    Now using array instead of queue

--- FlaskeAutomat/Program.cs
+++ FlaskeAutomat/Program.cs
@@ -1,139 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 
 namespace Flaskeautomat
 {
     class Program
     {
-        static Queue<int> _liquidDrinks = new Queue<int>();
-        static string beer = "Beer";
-        static string soda = "Soda";
-        private static int id;
-        
+        static int[] drinkArray = new int[2];
+        private static int _id = 0;
         static object _lock = new object();
         static Random _random = new Random();
-        
-        
+
+
         static void Main()
         {
-            Thread producerBeer = new Thread(ProducerBeer);
-            Thread producerSoda = new Thread(ProducerSoda);
-            Thread consumerBeer = new Thread(ConsumerBeer);
-            Thread consumerSoda = new Thread(ConsumerSoda);
-            
-            producerBeer.Start();
-            producerSoda.Start();
-            consumerBeer.Start();
-            consumerSoda.Start();
+
+            Thread producer = new Thread(Producer);
+            Thread consumer = new Thread(Consumer);
+
+            producer.Start();
+            consumer.Start();
         }
-        
-        static void ProducerBeer()
+
+        static void Producer()
         {
             while (true)
             {
-                lock (_lock)
+                Monitor.Enter(_lock);
+                try
                 {
-                    Monitor.Enter(_lock);
-                    try
+                    if (drinkArray[0] == 0 || drinkArray[1] == 0)
                     {
-                        id = _random.Next(1, 10);
-                        if (_liquidDrinks.Count < 10)
+                        _id = _random.Next(1, 2);
+                        if (_id == 1)
                         {
-                            _liquidDrinks.Enqueue(beer.Length);
-                            Console.WriteLine(beer + " PRODUCE " + id);
-                            Monitor.PulseAll(_lock);
+                            drinkArray.SetValue(1, 0);
+                            Console.WriteLine("PRODUCE id " + _id);
+                        }
+
+                        if (_id == 2)
+                        {
+                            drinkArray.SetValue(2, 1);
+                            Console.WriteLine("PRODUCE id " + _id);
                         }
                         else
                         {
-                            Monitor.Wait(_lock);
-                            Console.WriteLine("ProducerBeer is wating");
+                            Console.WriteLine("Producer Waits");
                         }
                     }
-                    finally
-                    {
-                        Monitor.Exit(_lock);
-                    }
 
+                    Monitor.PulseAll(_lock);
+                    Console.WriteLine(string.Join(", ", drinkArray) + "\n");
                     Thread.Sleep(_random.Next(200, 1000));
                 }
-            }
-        }
-        static void ProducerSoda()
-        {
-            while (true)
-            {
-                Monitor.Enter(_lock);
-                try
-                {
-                    id = _random.Next(1, 10);
-                    if (_liquidDrinks.Count < 10)
-                    {
-                        _liquidDrinks.Enqueue(soda.Length);
-                        Console.WriteLine(soda + " PRODUCE " + id);
-                        Monitor.PulseAll(_lock);
-                    }
-                    else
-                    {
-                        Monitor.Wait(_lock);
-                        Console.WriteLine("ProducerSoda is wating");
-                    }
-                }
                 finally
                 {
                     Monitor.Exit(_lock);
                 }
-                Thread.Sleep(_random.Next(200, 1000));
             }
         }
 
-        static void ConsumerBeer()
+        static void Consumer()
         {
             while (true)
             {
                 Monitor.Enter(_lock);
                 try
                 {
-                    if (_liquidDrinks.Count != 0)
+                    if (drinkArray[0] == 1 || drinkArray[1] == 2)
                     {
-                        if (_liquidDrinks.Count > 0)
+                        if (drinkArray[0] == 1)
                         {
-                            _liquidDrinks.Dequeue();
-                            Console.WriteLine(beer + " CONSUMER " + id);
+                            drinkArray.SetValue(0, 0);
+                            Console.WriteLine("CONSUME id " + _id);
+                            Monitor.PulseAll(_lock);
+                        }
+
+                        if (drinkArray[0] == 2)
+                        {
+                            drinkArray.SetValue(0, 1);
+                            Console.WriteLine("CONSUME id " + _id);
                             Monitor.PulseAll(_lock);
                         }
                         else
                         {
-                            Monitor.Wait(_lock);
-                            Console.WriteLine("ConsumeBeer is wating");
+                            Console.WriteLine("Consumer is wating");
                         }
-                    }
-                }
-                finally
-                {
-                    Monitor.Exit(_lock);
-                }
-                Thread.Sleep(_random.Next(200, 1000));
-            }
-        }
-
-        static void ConsumerSoda()
-        {
-            while (true)
-            {
-                Monitor.Enter(_lock);
-                try
-                {
-                    if (_liquidDrinks.Count > 0)
-                    {
-                        _liquidDrinks.Dequeue();
-                        Console.WriteLine(soda + " CONSUME " + id);
-                        Monitor.PulseAll(_lock);
-                    }
-                    else
-                    {
-                        Monitor.Wait(_lock);
-                        Console.WriteLine("ConsumeSoda is wating");
                     }
                 }
                 finally
